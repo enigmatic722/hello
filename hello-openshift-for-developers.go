87a0c7ba67e014d39bace28d60fd7679c3253f68
enigmatic722 <73819569+enigmatic722@users.noreply.github.com>
2023-11-12 15:56:16 +0800

    Update hello-openshift-for-developers.go
    222222

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello world!1111"
+		response = "Hello world!111122222"
 	}
 
 	fmt.Fprintln(w, response)
